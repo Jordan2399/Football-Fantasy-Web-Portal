13a1b55ca39fa6a75e4609c5f126ed6ffc4be07f
Sumit <11027576@STUD.HOCHSCHULE-HEIDELBERG.DE>
2024-02-01 22:12:25 +0100

    Chore: multer configured

--- match_services/src/resources/club/club.route.ts
+++ match_services/src/resources/club/club.route.ts
@@ -2,19 +2,18 @@ import { Router } from "express";
 import { ClubController, ClubValidation } from "./index.js";
 import { validateRequest } from "zod-express-middleware";
 import { UtilValidation } from "../../utils/index.js";
+import { ClubImage } from "../../vendor/multer/config.js";
 
 export namespace ClubRoute {
   export const Index = Router();
 
 
 
-  Index.get("/club", [ClubController.GetClub]);
-  Index.get("/club/:id", [ClubController.GetClub]);
-  // Index.get("/club/search", [ClubController.GetClub]);
-  Index.post("/club", [validateRequest(ClubValidation.CreateClub), ClubController.CreateClub]);
-  Index.delete("/club/:id", [UtilValidation.Id, ClubController.DeleteClub]);
-  Index.patch("/club/:id", [ClubController.UpdateClub]);
-
-
+ 
+  Index.post('/club', [
+    ClubImage.fields([
+      { name: 'image', maxCount: 1 }
+    ]), validateRequest(ClubValidation.CreateClub), ClubController.CreateClub
+  ]);
 
 }
--- match_services/src/routes/private/routes.private.route.ts
+++ match_services/src/routes/private/routes.private.route.ts
@@ -2,13 +2,15 @@ import { Router } from "express";
 import {
   AuthorizationRoute,
   PermissionRoute,
+  ClubRoute,
+
 } from "../../resources";
-import { ClubRoute } from "../../resources/club";
+// import { ClubRoute } from "../../resources/club";
 export namespace RoutesPrivateRoute {
   export const Index = Router();
   Index.use([
     AuthorizationRoute.Index,
     PermissionRoute.Index,
-    ClubRoute.Index,
+    ClubRoute.Index
   ]);
 }
