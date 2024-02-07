a34c36ee4b6eb001a7474e41cac81855456757f6
Jordan2399 <67420280+Jordan2399@users.noreply.github.com>
2024-02-07 13:18:41 +0100

    Chore: Add player screen, modifications to validation and other services added.

---
+++ client/src/services/account/account.service.ts
@@ -0,0 +1,13 @@
+import api from "@/services/config";
+import { AccountServiceType } from "@/services/account/type";
+
+export namespace AccountService {
+  export const history = async (): Promise<AccountServiceType.MatchHistory> => {
+    try {
+      const res = await api.get("/user/history");
+      return Promise.resolve(res.data);
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+}
